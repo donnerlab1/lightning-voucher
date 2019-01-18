b83e294cdbbf3d0b117d9e79b4bad14e10023d32
Konstantin Nick <kon@kon.ninja>
2019-01-18 09:13:31 +0100

    remove unnecessary stuff

--- wwwroot/voucher.js
+++ wwwroot/voucher.js
@@ -33,8 +33,11 @@ function decodePayment() {
 		},
 		error: function (jqXHR, textStatus, errorThrown) {
 
-			textfield = $("#buy-invoice-text");
-			textfield.show();
+		    $("#payment-div").hide();
+		    $("#decode-invoice-stuff").remove();
+		    textfield = $("#decode-invoice-text");
+		    textfield.show();
+		    textfield.append("<span id='decode-invoice-stuff' style='color:red'>INVALID INVOICE</span>");
 		}
 	});
 }
@@ -73,7 +76,6 @@ function getVoucher() {
 function useVoucher() {
     $("#decode-invoice-stuff").remove();
     const voucher_button = $("#use-voucher-button");
-    console.log(voucher_button)
     voucher_button.attr("disabled", "disabled")
     voucher_button.attr('value', "Please Wait...");
 	textfield = $("#decode-invoice-text");
@@ -123,117 +125,17 @@ function useVoucher() {
 		    voucher_button.removeAttr("disabled");
 		    voucher_button.attr('value', "Pay");
 		    $("#decode-invoice-stuff").remove();
+		    textfield = $("#decode-invoice-text");
+		    textfield.show();
+		    textfield.append("<span id='decode-invoice-stuff' style='color:red'>INVALID INVOICE</span>");
+
+		    $("#payment-div").hide();
 		}
 
 	});
 
 }
-function _arrayBufferToBase64( buffer ) {
-	var binary = '';
-	var bytes = new Uint8Array( buffer );
-	var len = bytes.byteLength;
-	for (var i = 0; i < len; i++) {
-		binary += String.fromCharCode( bytes[ i ] );
-	}
-	return window.btoa( binary );
-}
 
-function buyVoucher() {
-	const buy_item= {
-		buy_amt :$("#buy_amt").val(),
-		buy_sat : $("#buy_sat").val()
-	}
-	$.ajax({
-		type: "GET",
-		url: uri + "/buy/" + buy_item.buy_amt + "/" + buy_item.buy_sat,
-		cache: false,
-		success: function(data) {
-			console.log(data);
-			$("#buy-invoice-stuff").remove();
-			textfield = $("#buy-invoice-text");
-			textfield.show();
-			textfield.append("<span id='buy-invoice-stuff'>"+data.paymentRequest+"</span>");
-			$("#voucher-buy-payreq").val(data.paymentRequest);
-
-		},
-		error: function(jqXHR, textStatus, errorThrown) {
-			
-			textfield = $("#buy-invoice-text");
-			textfield.show();
-		}
-
-	});
-}
-
-function claimVoucher() {
-	const claim_item = {
-		claim_payreq: $("#voucher-buy-payreq").val()
-	};
-	$.ajax({
-		type: "GET",
-		url: uri + "/claim/" + claim_item.claim_payreq,
-		cache: false,
-		success: function(data) {
-			console.log(data);
-			if (data.errorCode === "Ok") {
-				$("#claim-div").show();
-				const tBody = $("#voucher-table");
-				$(tBody).empty();
-
-
-				$.each(data.vouchers,
-					function(key, item) {
-						
-						restSat = item.startSat - item.usedSat;
-						const tr = $("<tr></tr>")
-							.append($("<td></td>").text(item.id))
-							.append($("<td></td>").text(restSat));
-						tr.appendTo(tBody);
-					});
-			} else {
-				$("#claim-div").show();
-				const tBody = $("#voucher-table");
-				$(tBody).empty();
-				const tr = $("<tr></tr>")
-					.append($("<td></td>").text(data.errorCode));
-
-				tr.appendTo(tBody);
-			}
-		},
-		error: function(jqXHR, textStatus, errorThrown) {
-			$("#claim-div").show();
-		}
-
-	});
-}
-function getData() {
-  $.ajax({
-    type: "GET",
-    url: uri,
-    cache: false,
-    success: function(data) {
-      const tBody = $("#voucher-table");
-		
-	    $("#claim-div").show();
-	  console.log(data)
-      $(tBody).empty();
-
-
-      $.each(data, function(key, item) {
-	      restSat = item.startSat - item.usedSat;
-        const tr = $("<tr></tr>")
-          
-          .append($("<td></td>").text(item.id))
-		  .append($("<td></td>").text(restSat))
-          
-          ;
-
-        tr.appendTo(tBody);
-      });
-
-    }
-  });
-}
 
 
 
